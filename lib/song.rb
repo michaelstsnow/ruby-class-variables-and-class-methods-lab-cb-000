228730ac190de6724c2cc6c80da61aba246d35ed
michaelstsnow <michael.st.snow@gmail.com>
2018-11-17 22:11:46 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,29 @@
+class Song
+  attr_accessor(:name,:artist,:genre)
+  @@count=0
+  @@artists=[]
+  @@genres=[]
+  @@artist_count={}
+  @@genre_count={}
+  def initialize(name,artist,genre)
+    @name=name
+    @artist=artist
+    @genre=genre
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+
+  def self.count
+    return @@count
+  end
+
+  def self.genres
+    return @@genres.uniq
+  end
+
+  def self.artists
+    return @@artists.uniq
+  end
+
+end
